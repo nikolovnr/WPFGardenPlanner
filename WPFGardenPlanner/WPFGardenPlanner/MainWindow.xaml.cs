fc930b8706409a8d41bbcfc7502cafaea7da17e0
Nathalie\Nathalie Desrosiers <n.desrosiers@yahoo.com>
2016-09-09 13:26:17 -0400

    no message

--- WPFGardenPlanner/WPFGardenPlanner/MainWindow.xaml.cs
+++ WPFGardenPlanner/WPFGardenPlanner/MainWindow.xaml.cs
@@ -14,16 +14,15 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WPFGardenPlanner
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+{   
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            Title = "GardenPlanner";
         }
 
+       
     }
 }
