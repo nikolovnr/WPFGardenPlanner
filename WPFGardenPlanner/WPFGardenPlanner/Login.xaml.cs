cd65b251ed14c4003a57d6720b3409aada35639e
Nathalie\Nathalie Desrosiers <n.desrosiers@yahoo.com>
2016-09-12 11:06:11 -0400

    * saving local changes before a pull

---
+++ WPFGardenPlanner/WPFGardenPlanner/Login.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Diagnostics;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFGardenPlanner
+{
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+            Title = "Login window";
+
+        }
+
+        private void buttonLogIn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+            //TODO: Change the address of the URL in XAML
+        }
+    }
+    public class WaterMarkTextHelper : DependencyObject
+    {
+        #region Attached Properties
+
+        public static bool GetIsMonitoring(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsMonitoringProperty);
+        }
+
+        public static void SetIsMonitoring(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsMonitoringProperty, value);
+        }
+
+        public static readonly DependencyProperty IsMonitoringProperty =
+            DependencyProperty.RegisterAttached("IsMonitoring", typeof(bool), typeof(WaterMarkTextHelper), new UIPropertyMetadata(false, OnIsMonitoringChanged));
+
+
+        public static bool GetWatermarkText(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(WatermarkTextProperty);
+        }
+
+        public static void SetWatermarkText(DependencyObject obj, string value)
+        {
+            obj.SetValue(WatermarkTextProperty, value);
+        }
+
+        public static readonly DependencyProperty WatermarkTextProperty =
+            DependencyProperty.RegisterAttached("WatermarkText", typeof(string), typeof(WaterMarkTextHelper), new UIPropertyMetadata(string.Empty));
+
+
+        public static int GetTextLength(DependencyObject obj)
+        {
+            return (int)obj.GetValue(TextLengthProperty);
+        }
+
+        public static void SetTextLength(DependencyObject obj, int value)
+        {
+            obj.SetValue(TextLengthProperty, value);
+
+            if (value >= 1)
+                obj.SetValue(HasTextProperty, true);
+            else
+                obj.SetValue(HasTextProperty, false);
+        }
+
+        public static readonly DependencyProperty TextLengthProperty =
+            DependencyProperty.RegisterAttached("TextLength", typeof(int), typeof(WaterMarkTextHelper), new UIPropertyMetadata(0));
+
+        #endregion
+
+        #region Internal DependencyProperty
+
+        public bool HasText
+        {
+            get { return (bool)GetValue(HasTextProperty); }
+            set { SetValue(HasTextProperty, value); }
+        }
+
+        private static readonly DependencyProperty HasTextProperty =
+            DependencyProperty.RegisterAttached("HasText", typeof(bool), typeof(WaterMarkTextHelper), new FrameworkPropertyMetadata(false));
+
+        #endregion
+
+        #region Implementation
+
+        static void OnIsMonitoringChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is TextBox)
+            {
+                TextBox txtBox = d as TextBox;
+
+                if ((bool)e.NewValue)
+                    txtBox.TextChanged += TextChanged;
+                else
+                    txtBox.TextChanged -= TextChanged;
+            }
+            else if (d is PasswordBox)
+            {
+                PasswordBox passBox = d as PasswordBox;
+
+                if ((bool)e.NewValue)
+                    passBox.PasswordChanged += PasswordChanged;
+                else
+                    passBox.PasswordChanged -= PasswordChanged;
+            }
+        }
+
+        static void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox txtBox = sender as TextBox;
+            if (txtBox == null) return;
+            SetTextLength(txtBox, txtBox.Text.Length);
+        }
+
+        static void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passBox = sender as PasswordBox;
+            if (passBox == null) return;
+            SetTextLength(passBox, passBox.Password.Length);
+        }
+
+        #endregion
+    }
+}
