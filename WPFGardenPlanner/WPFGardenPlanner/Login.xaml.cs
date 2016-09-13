83f824536dc750472d051fe252fc596b1594f5f9
Nathalie\Nathalie Desrosiers <n.desrosiers@yahoo.com>
2016-09-13 12:07:34 -0400

    Watermark on textbox

--- WPFGardenPlanner/WPFGardenPlanner/Login.xaml.cs
+++ WPFGardenPlanner/WPFGardenPlanner/Login.xaml.cs
@@ -22,7 +22,6 @@ namespace WPFGardenPlanner
         {
             InitializeComponent();
             Title = "Login window";
-
         }
 
         private void buttonLogIn_Click(object sender, RoutedEventArgs e)
@@ -36,110 +35,31 @@ namespace WPFGardenPlanner
             e.Handled = true;
             //TODO: Change the address of the URL in XAML
         }
-    }
-    public class WaterMarkTextHelper : DependencyObject
-    {
-        #region Attached Properties
 
-        public static bool GetIsMonitoring(DependencyObject obj)
+        private void textBoxEMail_LostFocus(object sender, RoutedEventArgs e)
         {
-            return (bool)obj.GetValue(IsMonitoringProperty);
-        }
-
-        public static void SetIsMonitoring(DependencyObject obj, bool value)
-        {
-            obj.SetValue(IsMonitoringProperty, value);
-        }
-
-        public static readonly DependencyProperty IsMonitoringProperty =
-            DependencyProperty.RegisterAttached("IsMonitoring", typeof(bool), typeof(WaterMarkTextHelper), new UIPropertyMetadata(false, OnIsMonitoringChanged));
-
-
-        public static bool GetWatermarkText(DependencyObject obj)
-        {
-            return (bool)obj.GetValue(WatermarkTextProperty);
-        }
-
-        public static void SetWatermarkText(DependencyObject obj, string value)
-        {
-            obj.SetValue(WatermarkTextProperty, value);
-        }
-
-        public static readonly DependencyProperty WatermarkTextProperty =
-            DependencyProperty.RegisterAttached("WatermarkText", typeof(string), typeof(WaterMarkTextHelper), new UIPropertyMetadata(string.Empty));
-
-
-        public static int GetTextLength(DependencyObject obj)
-        {
-            return (int)obj.GetValue(TextLengthProperty);
-        }
-
-        public static void SetTextLength(DependencyObject obj, int value)
-        {
-            obj.SetValue(TextLengthProperty, value);
-
-            if (value >= 1)
-                obj.SetValue(HasTextProperty, true);
-            else
-                obj.SetValue(HasTextProperty, false);
-        }
-
-        public static readonly DependencyProperty TextLengthProperty =
-            DependencyProperty.RegisterAttached("TextLength", typeof(int), typeof(WaterMarkTextHelper), new UIPropertyMetadata(0));
-
-        #endregion
-
-        #region Internal DependencyProperty
-
-        public bool HasText
-        {
-            get { return (bool)GetValue(HasTextProperty); }
-            set { SetValue(HasTextProperty, value); }
-        }
-
-        private static readonly DependencyProperty HasTextProperty =
-            DependencyProperty.RegisterAttached("HasText", typeof(bool), typeof(WaterMarkTextHelper), new FrameworkPropertyMetadata(false));
-
-        #endregion
-
-        #region Implementation
-
-        static void OnIsMonitoringChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            if (d is TextBox)
+            if(string.IsNullOrEmpty(textBoxEMail.Text))
             {
-                TextBox txtBox = d as TextBox;
-
-                if ((bool)e.NewValue)
-                    txtBox.TextChanged += TextChanged;
-                else
-                    txtBox.TextChanged -= TextChanged;
-            }
-            else if (d is PasswordBox)
-            {
-                PasswordBox passBox = d as PasswordBox;
-
-                if ((bool)e.NewValue)
-                    passBox.PasswordChanged += PasswordChanged;
-                else
-                    passBox.PasswordChanged -= PasswordChanged;
+                textBoxEMail.Visibility = System.Windows.Visibility.Collapsed;
+                waterMarkedEMail.Visibility = System.Windows.Visibility.Visible;
             }
         }
 
-        static void TextChanged(object sender, TextChangedEventArgs e)
+        private void waterMarkedEMail_GotFocus(object sender, RoutedEventArgs e)
         {
-            TextBox txtBox = sender as TextBox;
-            if (txtBox == null) return;
-            SetTextLength(txtBox, txtBox.Text.Length);
+            waterMarkedEMail.Visibility = System.Windows.Visibility.Collapsed;
+            textBoxEMail.Visibility = System.Windows.Visibility.Visible;
+            textBoxEMail.Focus();
         }
 
-        static void PasswordChanged(object sender, RoutedEventArgs e)
+        private void passwordBox_LostFocus(object sender, RoutedEventArgs e)
         {
-            PasswordBox passBox = sender as PasswordBox;
-            if (passBox == null) return;
-            SetTextLength(passBox, passBox.Password.Length);
+
         }
 
-        #endregion
-    }
+        private void waterMarkedPassword_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+    } 
 }
