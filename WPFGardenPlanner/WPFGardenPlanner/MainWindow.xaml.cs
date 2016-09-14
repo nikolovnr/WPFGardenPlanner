4debb9ed790ce5e7e78d81539ad40689b51f2052
Nikolay\Nikolay Nikolov <nikolovnr@yahoo.com>
2016-09-14 09:09:04 -0700

    * Seeding a plant (put images on grid) * Save garden as image (garden.png) * Print garden

--- WPFGardenPlanner/WPFGardenPlanner/MainWindow.xaml.cs
+++ WPFGardenPlanner/WPFGardenPlanner/MainWindow.xaml.cs
@@ -26,8 +26,8 @@ namespace WPFGardenPlanner
     public partial class MainWindow : Window
     {
         bool isDrawingBeds = false;
-        Button btnClicked;
-        
+        string SeedPlant;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -58,12 +58,18 @@ namespace WPFGardenPlanner
                 }
                 else
                 {
-                    var ss = someButton.Content;
+                    //var ss = someButton.Content;
                     Image imgControl = new Image();
-                    var bitmapImage = new BitmapImage(new Uri(@"C:\Users\Nikolay Nikolov\Documents\WPFGardenPlanner2\WPFGardenPlanner\WPFGardenPlanner\Pictures\Plants\Beet.png"));
+                    //var bitmapImage = new BitmapImage(new Uri(@"C:\Users\Nikolay Nikolov\Documents\WPFGardenPlanner2\WPFGardenPlanner\WPFGardenPlanner\Pictures\Plants\Beet.png"));
 
+
+                    //imgControl.Source = bitmapImage;
+
+
+                    var bitmapImage = new BitmapImage(new Uri(SeedPlant));
 
                     imgControl.Source = bitmapImage;
+
                     someButton.Content = imgControl;
 
                    
@@ -78,7 +84,59 @@ namespace WPFGardenPlanner
             if (someButton != null)
             {
                 isDrawingBeds = false;
-               
+                SeedPlant = (string)someButton.Tag;
+            }
+        }
+
+        private void SaveGardenAsImage(object sender, RoutedEventArgs e)
+        {
+            Rect bounds = VisualTreeHelper.GetDescendantBounds(grdGardenPlan);
+            double dpi = 96d;
+
+
+            RenderTargetBitmap rtb = new RenderTargetBitmap((int)bounds.Width, (int)bounds.Height, dpi, dpi, System.Windows.Media.PixelFormats.Default);
+
+
+            DrawingVisual dv = new DrawingVisual();
+            using (DrawingContext dc = dv.RenderOpen())
+            {
+                VisualBrush vb = new VisualBrush(grdGardenPlan);
+                dc.DrawRectangle(vb, null, new Rect(new Point(), bounds.Size));
+            }
+
+            rtb.Render(dv);
+
+            BitmapEncoder pngEncoder = new PngBitmapEncoder();
+            pngEncoder.Frames.Add(BitmapFrame.Create(rtb));
+
+            try
+            {
+                System.IO.MemoryStream ms = new System.IO.MemoryStream();
+
+                pngEncoder.Save(ms);
+                ms.Close();
+
+                System.IO.File.WriteAllBytes("garden.png", ms.ToArray());
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void PrintGarden(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                PrintDialog dialog = new PrintDialog();
+
+                if (dialog.ShowDialog() != true)
+                    return;
+                dialog.PrintVisual(grdGardenPlan, "IFMS Print Screen");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Print Screen", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
     }
