079c709c6546db692d6a9aadadcc8e295eaf3f2b
Nikolay\Nikolay Nikolov <nikolovnr@yahoo.com>
2016-09-16 19:04:31 -0700

    no message

--- WPFGardenPlanner/WPFGardenPlanner/Database.cs
+++ WPFGardenPlanner/WPFGardenPlanner/Database.cs
@@ -28,11 +28,10 @@ namespace GardenPlanner
     public class Bed
     {
         public int BedId { get; set; }
-        public int GardenId { get; set; }
-        public string Name { get; set; }
-        public int CoordinateX { get; set; }
-        public int CoordinateY { get; set; }
-        public string PicSource { get; set; }
+        public string BedName { get; set; }
+        public int Garden_Id { get; set; }
+        public string Shape { get; set; }
+        //public numeric? Size { get; set; }
     }
 
     public class Plant
